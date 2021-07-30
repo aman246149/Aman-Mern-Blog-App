785b403f26c0aa8bf1f964b018b0a125a00a1373
Aman <amanthapliyal14@gmail.com>
2021-07-30 09:03:48 +0530

    made homepage fully responsive

--- src/Components/BlogCards/BlogCards.js
+++ src/Components/BlogCards/BlogCards.js
@@ -23,13 +23,34 @@ function BlogCards() {
 
   }
 
+  function changeDimensionsfor1024(){
+     if(window.outerWidth<1025){
+   setImageDime({
+        height: "40vh",
+        width: "100%",
+    });
+    }else {
+      setImageDime({
+        height: "22vh",
+        width: "20vw",
+      });
+    }
+  }
+
   useEffect(() => {
+    if (window.outerWidth<650 ) {
+      changeDimensions()
+    }else if(window.outerWidth<1025){
+      changeDimensionsfor1024()
+    }
+
     window.addEventListener("resize", changeDimensions);
     console.log("Render", imageDimen);
 
     return () => {
       window.removeEventListener("resize", changeDimensions);
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [imageDimen]);
 
   return (
@@ -50,7 +71,7 @@ function BlogCards() {
             "url('https://images.unsplash.com/photo-1627552113979-aae121b0e7bc?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0NHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')",
         }}
       >
-        <h6>How to optimize an overload tool in your organization</h6>
+        {/* <h6>How to optimize an overload tool in your organization</h6> */}
       </div>
     </div>
   );
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,14 +1,10 @@
 import React from "react";
 import BlogMotto from "../../Components/Blog Moto/BlogMotto";
 import BlogCards from "../../Components/BlogCards/BlogCards";
-import Navbar from "../../Components/Navbar/Navbar";
 
 function HomePage() {
   return (
     <>
-      {/*fixed top bar */}
-     
-
       {/* blog motto section */}
 
       <BlogMotto />
