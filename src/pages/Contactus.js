51650f3183ae1ec1d7b7ea8c6f96c5070f1c14a9
Aman <amanthapliyal14@gmail.com>
2021-08-02 14:11:40 +0530

    made loader

--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -1,37 +1,93 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import "./AllPage.css";
+import axios from "axios";
 
 function Login() {
+  const [createaccount, setcreateaccount] = useState(false);
 
-  const [createaccount, setcreateaccount] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorstate, setErrorState] = useState("");
+  const [loaderdata, setLoader] = useState(false);
 
-  const[email,setEmail]=useState("")
-  const[password,setPassword]=useState("")
-
-
-
-  function handleCreateAccount(){
+  function handleCreateAccount() {
     if (createaccount) {
-        setcreateaccount(false)
-      }else{
-        setcreateaccount(true)
-
-      }
+      setcreateaccount(false);
+    } else {
+      setcreateaccount(true);
+    }
   }
 
-  function signUpActivity(e){
-      console.log(email,password)
+  function signUpActivity(e) {
+    setLoader(true);
+    console.log(email, password);
+    const data = JSON.stringify({
+      email: email,
+      password: password,
+    });
+
+    console.log(data);
+    if (createaccount === true) {
+      axios
+        .post("http://localhost:3000/api/user/login", data, {
+          headers: { "Content-Type": "application/json" },
+        })
+        .then((response) => setLoader(false))
+        .catch((err) => {
+          setErrorState("email or password does not match");
+          setLoader(false);
+        });
+    } else {
+      console.log("regisre");
+      axios
+        .post("http://localhost:3000/api/user/register", data, {
+          headers: { "Content-Type": "application/json" },
+        })
+        .then((response) => setLoader(false))
+        .catch((err) => {
+          setErrorState("already exist email");
+          setLoader(false);
+        });
+    }
+
+    //handling error state
+    setTimeout(() => setErrorState(""), 3000);
   }
 
   return (
     <div className="section">
+      <h2 className="loading">{loaderdata===true?"Loading":null}</h2>
       <div className="logincontainer">
+        <div className={`${errorstate !== "" ? "errorbox" : "nonactive"}`}>
+          {errorstate !== "" ? errorstate : ""}
+        </div>
         <label htmlFor="email">Email</label>
-        <input type="email" required placeholder="email" onChange={(data)=>setEmail(data.target.value)}/>
+        <input
+          type="email"
+          required
+          placeholder="email"
+          onChange={(data) => setEmail(data.target.value)}
+        />
         <label htmlFor="password">Password</label>
-        <input type="password" required onChange={(data)=>setPassword(data.target.value)}/>
-        <input type="submit" value={createaccount===false?"Create Account":"Login"} onClick={signUpActivity}/>
-        <h5>{createaccount===false?"Dont have an account?":"Already Have An Account"} <span className="createaccount" onClick={handleCreateAccount}>{createaccount===false?"Create Account":"Login"}</span></h5>
+        <input
+          type="password"
+          required
+          placeholder="password"
+          onChange={(data) => setPassword(data.target.value)}
+        />
+        <input
+          type="submit"
+          value={createaccount === false ? "Create Account" : "Login"}
+          onClick={signUpActivity}
+        />
+        <h5>
+          {createaccount === false
+            ? "Dont have an account?"
+            : "Already Have An Account"}{" "}
+          <span className="createaccount" onClick={handleCreateAccount}>
+            {createaccount === false ? "Create Account" : "Login"}
+          </span>
+        </h5>
       </div>
     </div>
   );
