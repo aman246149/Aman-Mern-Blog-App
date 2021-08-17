7a4ece9f194117c8402db16daa5231b108d38bd5
Aman <amanthapliyal14@gmail.com>
2021-08-17 21:08:06 +0530

    adding github actions

--- src/Components/Blog Moto/BlogMotto.js
+++ src/Components/Blog Moto/BlogMotto.js
@@ -7,7 +7,7 @@ function BlogMotto() {
             <h5>BLOG</h5>
             <h1>OUR LATEST AND MOST INSIGHTFULL</h1>
 
-            <p>We love to share! On this page, we write about our client & venture engagements, our successes - but also our hiccups and what we learned from them. Consider these updates a sneak peek into our buzzing minds.</p>
+            <p>Thanks for reading this Blog,This blog provides you latest information about webdevelopment,technology ,tips tricks and Dsa Skills</p>
         </section>
     )
 }
